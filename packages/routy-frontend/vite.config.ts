3026e41451d96068a3cd13daab998017dc8a4664
Artem Prokop <artem13.prokop@gmail.com>
2024-06-08 18:10:50 +0200

    feat: dynamic start options

--- packages/routy-frontend/vite.config.ts
+++ packages/routy-frontend/vite.config.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { config } from 'dotenv'
+
+config()
 
 export default defineConfig(() => {
   const resolve = {
@@ -18,12 +21,12 @@ export default defineConfig(() => {
     plugins: [react(), TanStackRouterVite()],
     server: {
       port: process.env.DEV_PORT ? Number(process.env.DEV_PORT) : 3000,
-      host: '127.0.0.1',
+      host: process.env.DEV_HOST ?? '127.0.0.1',
       watch: {},
     },
     preview: {
       port: process.env.PROD_PORT ? Number(process.env.PROD_PORT) : 3001,
-      host: '127.0.0.1',
+      host: process.env.PROD_HOST ?? '127.0.0.1',
     },
   };
 });
